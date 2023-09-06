952480f36e2a3fe7cd9140eaee0d03db6a4b4154
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2023-09-07 01:20:55 +0300

    регулировка текста (размер шрифта и перенос строк) в зависимости от рамок ячеек

--- img_crt.py
+++ img_crt.py
@@ -25,7 +25,6 @@ def list_tasks(text):
         task_dict[task_time] = line
 
     sorted_dict = dict(sorted(task_dict.items()))
-    print(sorted_dict)
     res = ''
     for el in sorted_dict.values():
         res += el + '/'
@@ -39,22 +38,63 @@ def img_crt(type_img, text):
     lines = lines.split('/')
     num_lines = len(lines)
     if type_img == 'month':
+
         main_img = Image.open("img/month.png")
         idraw = ImageDraw.Draw(main_img)
-        cur_size = int(203 / (num_lines + 1))
+        cur_size = 203 // (num_lines + 1)  # размер шрифта
+        for i in range(len(lines)):
+            line = lines[i]
+            need_len = cur_size // 3
+            if need_len >= len(line):
+                new_line = line + '\n'
+            else:
+                num_lines += 1
+                if (len(line) - need_len) <= 5:
+                    new_line = line[:-5].strip() + '\n' + line[-5:].strip() + '\n'
+                else:
+                    new_line = line[:-len(line) + need_len].strip() + '\n' + line[-(
+                            len(line) - need_len) + 2:].strip() + '\n'
+                if i == len(lines) - 1:
+                    new_line = new_line[:-1]
+            lines[i] = new_line
+        cur_size = 203 // (num_lines + 1)
         font = ImageFont.truetype("Inter-V.ttf", size=cur_size)
         x_line = 224
         y_line = 189
-        for line in lines:
-            idraw.text((x_line, y_line), line.strip(), font=font, fill=(0, 0, 0, 255))
-            y_line += cur_size + cur_size / num_lines
+
+        res_text = ''.join(lines)
+        idraw.text((x_line, y_line), res_text, font=font, fill=(0, 0, 0, 255))  # пишет в ячейку
+
+        # обработка и вывод текста отдельно по строчкам (изначальный вариант).
+        # было принято решение внутри одного текста менять строчки, но не разделять их и не выводить отдельно
+
+        # for line in lines:
+        #     # print(238 // cur_size)
+        #     # if 238 * 2 // cur_size <= len(line):
+        #     #     cur_size -= 5
+        #     # else:
+        #     #     cur_size = int(203 / (num_lines + 1))
+        #     #     print(line, 'DONE')
+        #     # print(line, cur_size)
+        #
+        #     need_len = cur_size // 5
+        #     print(need_len, len(line))
+        #     if need_len <= len(line):
+        #         idraw.text((x_line, y_line), line.strip(), font=font, fill=(0, 0, 0, 255))  # пишет в ячейку
+        #         y_line += cur_size + cur_size / num_lines  # коорд новой строчки по оси игрек
+        #     else:
+        #         if (len(line) - need_len) <= 5:
+        #             idraw.text((x_line, y_line), line[:5].strip(), font=font, fill=(0, 0, 0, 255))  # пишет в ячейку
+        #             y_line += cur_size + cur_size / num_lines  # коорд новой строчки по оси игрек
+        #             idraw.text((x_line, y_line), line[-5:].strip(), font=font, fill=(0, 0, 0, 255))  # пишет в ячейку
+        #             num_lines += 1
     elif type_img == 'day':
         # main_img = Image.open("img/day.png")
         pass
     main_img.save('img/res_month_img.png')
 
 
-img_crt('month', 'first/second 15:00/6')
+img_crt('month', 'первое 10:00/дз/обнять алину 13:00/мрмяу 13:45')
 
 # print(list_tasks('first 12:00/sec 10:00'))
 
